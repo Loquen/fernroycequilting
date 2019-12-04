3fa53d4d792bb71cc6e5a91d72f3a7041a11f8da
Loquen Jones <lrjones416@gmail.com>
2019-12-04 12:52:59 -0800

    configure MongoDB/mongoose

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+
+/**************** M I D D L E W A R E ******************/ 
 app.use(logger('dev'));
 app.use(express.json());
 
-// Configure both serve-favicon & static middlewares
-// to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Put API routes here, before the "catch all" route
+/**************** A P I  R O U T E S ******************/ 
 
-// The following "catch all" route (note the *)is necessary
-// for a SPA's client-side routing to properly work
+/**************** C A T C H  A L L  R O U T E  ******************/ 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
