360ef5fbd89e0bc3919015d35b932438cb94adf6
Loquen Jones <lrjones416@gmail.com>
2020-01-06 09:02:49 -0800

    clean up code for event controller

--- controllers/events.js
+++ controllers/events.js
@@ -7,29 +7,24 @@ module.exports = {
   update,
 };
 
+// Return All Events
 async function getAllEvents(req, res) {
   let events = await Event.find({});
   return res.json(events);
 }
 
+// Add New Event
 async function create(req, res) {
-
-  // return Event.create(req.body)
-  //   .then(() => (res.redirect('/event')))
-                  
-  var event = new Event(req.body);
-
-  // console.log(event);
+  let event = new Event(req.body);
   event.save(function(err){
     if(err) {
-      console.log(err);
       return res.json({ error: err });
     }
-    console.log(event);
     return res.json(event);
   });
 }
 
+// Edit Existing Event
 async function update(req, res) {
 
 }
