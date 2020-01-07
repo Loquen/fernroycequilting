e343d5f4cef5a6f7a6b2c68f24eec73f2b0a0fda
Loquen Jones <lrjones416@gmail.com>
2020-01-07 08:48:05 -0800

    add workshop form to upload page, succesful upload

---
+++ src/components/UploadForms/WorkshopForm.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Button, Alert } from 'react-bootstrap';
+import workshopService from '../../utils/workshopService';
+
+class WorkshopForm extends Component {
+
+  state = {
+    title: '',
+    url: '',
+    image_text: '',
+    details: '',
+    show: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    await workshopService.create(
+      this.state.title, 
+      this.state.url, 
+      this.state.image_text,
+      this.state.details
+    );
+    this.setState({show: true})
+  }
+
+  render(){
+    return (
+      <>
+        <header><h1 className='title'>Create A New Workshop</h1></header>
+        <Alert show={this.state.show} variant='success'>
+          Workshop successfully uploaded
+        </Alert>
+        <Container>        
+          <form className='form-horizontal' onSubmit={this.handleSubmit} >
+            <div className='form-group'>
+              <div className='col-sm-12'>
+                <input 
+                  type='text' 
+                  className='form-control' 
+                  placeholder='Title' 
+                  value={this.state.title} 
+                  name='title' 
+                  onChange={this.handleChange} 
+                />
+              </div>
+            </div>
+            <div className='form-group'>
+              <div className='col-sm-12'>
+                <input 
+                  type='text' 
+                  className='form-control' 
+                  placeholder='Image URL' 
+                  value={this.state.url} 
+                  name='url' 
+                  onChange={this.handleChange} 
+                />
+              </div>
+            </div>
+            <div className='form-group'>
+              <div className='col-sm-12'>
+                <input 
+                  type='text' 
+                  className='form-control' 
+                  placeholder='Image Blurb' 
+                  value={this.state.image_text} 
+                  name='image_text' 
+                  onChange={this.handleChange} 
+                />
+              </div>
+            </div>
+            <div className='form-group'>
+              <div className='col-sm-12'>
+                <input 
+                  type='text' 
+                  className='form-control' 
+                  placeholder='Description' 
+                  value={this.state.details} 
+                  name='details' 
+                  onChange={this.handleChange} 
+                />
+              </div>
+            </div>
+            <div className='form-group'>
+              <div className='col-sm-12 text-center'>
+                <Button className='btn btn-default' type='submit'>Upload</Button>&nbsp;&nbsp;&nbsp;
+                <Link to='/'>Cancel</Link>
+              </div>
+            </div>
+          </form>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default WorkshopForm;
--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Container, Jumbotron } from 'react-bootstrap';
 import EventForm from '../../components/UploadForms/EventForm';
+import WorkshopForm from '../../components/UploadForms/WorkshopForm';
 
 const UploadPage = () => {
   return (
     <Container>
       <Jumbotron>
-        <h1 className='title'>Upload Page!</h1>
+        <h1 className='title'>Upload Page</h1>
       </Jumbotron>
       <EventForm />
+      <WorkshopForm />
     </Container>
   );
 }
