4779dbe787486545a78a8c27790d4432e1c6eeb4
Loquen Jones <lrjones416@gmail.com>
2020-02-07 11:54:29 -0800

    fix footer spacing, positioning and content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,17 @@ import './Footer.css';
 function Footer(props) {
   return (
     <div className='footer'>
-      <div className='insta'>Instagram Feed</div>
+      <div className='insta'>
+        <a href='https://instagram.com/fern_royce'>
+          <img src="four_patch.jpg" alt="four patch quilt"/>  
+          <img src="american_quilter.jpg" alt="four patch quilt"/>  
+          <img src="pastel_blue.jpg" alt="four patch quilt"/>  
+          <img src="pin_cushion.jpg" alt="four patch quilt"/>  
+        </a>
+      </div>
       <div className='subscribe'>
-        <input type='email' placeholder='email'/>
+        <p>Subscribe to my mailing list to stay up to date!</p>
+        <input type='email' placeholder='Enter your email'/>
         <Button>Subscribe</Button>
       </div>
     </div>
