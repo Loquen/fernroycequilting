95a8d57125efd890d66ff60b876f4727593c4f40
Loquen Jones <lrjones416@gmail.com>
2020-03-10 08:33:17 -0700

    copyright in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,25 +4,30 @@ import SubscribePage from '../SubscribePage/SubscribePage';
 
 function Footer(props) {
   return (
-    <div className='footer'>
-      <div className='insta'>
-        <a href='https://instagram.com/fern_royce'>
-          <img src='four_patch.jpg' alt='four patch quilt'/>  
-          <img src='american_quilter.jpg' alt='American quilter book'/>  
-          <img src='pastel_blue.jpg' alt='pastel blue quilt'/>  
-          <img src='pin_cushion.jpg' alt='pin cushion'/>  
-        </a>
-      </div>
-      <div className='social'>
-        <h4>Follow me on social media:</h4>
-        <div>
-          <a className='App-link' href='https://www.pinterest.com/fernroyce/'><i className='fa fa-pinterest fa-3x'/></a>
-          <a className='App-link' href='https://www.facebook.com/fernroycequilts/'><i className='fa fa-facebook fa-3x' /></a>
-          <a className='App-link' href='https://www.instagram.com/fern_royce'><i className='fa fa-instagram fa-3x'/></a>
+    <div>
+      <div className='footer'>
+        <div className='insta'>
+          <a href='https://instagram.com/fern_royce'>
+            <img src='four_patch.jpg' alt='four patch quilt'/>  
+            <img src='american_quilter.jpg' alt='American quilter book'/>  
+            <img src='pastel_blue.jpg' alt='pastel blue quilt'/>  
+            <img src='pin_cushion.jpg' alt='pin cushion'/>  
+          </a>
+        </div>
+        <div className='social'>
+          <h4>Follow me on social media:</h4>
+          <div>
+            <a className='App-link' href='https://www.pinterest.com/fernroyce/'><i className='fa fa-pinterest fa-3x'/></a>
+            <a className='App-link' href='https://www.facebook.com/fernroycequilts/'><i className='fa fa-facebook fa-3x' /></a>
+            <a className='App-link' href='https://www.instagram.com/fern_royce'><i className='fa fa-instagram fa-3x'/></a>
+          </div>
+        </div>
+        <div className='subscribe'>
+          <SubscribePage />
         </div>
       </div>
-      <div className='subscribe'>
-        <SubscribePage />
+      <div className='copyright'>
+        © 2020 <a href='https://github.com'>Loquen Jones</a>
       </div>
     </div>
   );
